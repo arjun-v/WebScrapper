30d0db9b6ed38f1b5ffc3a48756d122224690190
Arjun V <arjunv@Arjuns-MacBook-Pro.local>
2013-06-09 15:57:22 +0530

    commit

--- com/jtidy/scrapper/Scrapper.java
+++ com/jtidy/scrapper/Scrapper.java
@@ -1,53 +1,96 @@
 package com.jtidy.scrapper;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.nutch.parse.Parser;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.tidy.Tidy;
 
-public class Scrapper {
-
-	public static void main(String[] args) {
+public class Scrapper implements Parser {
+	
+	private static Tidy tidy;
+	private Configuration conf;
+	
+	private Tidy GetTidy(){
+		if (this.tidy != null) {
+			return this.tidy;
+		}
+		Tidy tidy = new Tidy();
+		tidy.setXHTML(true); 
+		tidy.setShowErrors(0);
+		tidy.setQuiet(true);
+		tidy.setShowWarnings(false);
+		this.tidy = tidy;
+		return this.tidy;
+	}
+	
+	private HashMap<String,String> Scrape(InputStream in) {
 		try{
 			
-			File file = new File("/usr/local/apache-nutch-1.6/dump_folder2/test");
-			InputStream in = new FileInputStream(file);
-			Tidy tidy = new Tidy();
-			tidy.setXHTML(true); 
-			tidy.setShowErrors(0);
-			tidy.setQuiet(true);
-			tidy.setShowWarnings(false);
+			Tidy tidy = GetTidy();
 			Document document = tidy.parseDOM(in, null);
 			XPath xpath = XPathFactory.newInstance().newXPath();
 			
+			HashMap<String,String> map = new HashMap<String, String>();
+			
 			//*[@id="overview-top"]/h1/span[1]
-			Element itemprop = (Element) xpath.compile("//*[@id='overview-top']/h1/span[1]").evaluate(document, XPathConstants.NODE);
-			System.out.print(itemprop.getFirstChild().getNodeValue() + "\t");
+			Element movie = (Element) xpath.compile("//*[@id='overview-top']/h1/span[1]").evaluate(document, XPathConstants.NODE);
+			map.put("movie", movie.getFirstChild().getNodeValue());
 			
 			//*[@id="overview-top"]/div[3]/div[3]/strong/span
 			Element rating = (Element) xpath.compile("//*[@id='overview-top']/div[3]/div[3]/strong/span").evaluate(document, XPathConstants.NODE);
-			System.out.print(rating.getFirstChild().getNodeValue() + "\t");
+			map.put("rating", rating.getFirstChild().getNodeValue());
 
 			//*[@id="overview-top"]/div[4]/a/span
 			Element director = (Element) xpath.compile("//*[@id='overview-top']/div[4]/a/span").evaluate(document, XPathConstants.NODE);
-			System.out.print(director.getFirstChild().getNodeValue() + "\n");
+			map.put("director", director.getFirstChild().getNodeValue());
+
+			return map;
 			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (XPathExpressionException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return null;
 	}
+
+	@Override
+	public ParseResult getParse(Content content) {
+		InputStream in = new ByteArrayInputStream(content.getContent());
+		HashMap<String,String> map = Scrape(in);
+		try {
+			in.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ParseResult result = new ParseResult(content.getUrl());
+
+		for (String key : map.keySet()) {
+			result.put(key,new ParseText(map.get(key)),new ParseData());
+		}
+		
+		return result;
+		
+	}
+	
+	public Configuration getConf() {
+	    return conf;
+	  }
+
+	  public void setConf(Configuration conf) {
+	    this.conf = conf;
+	  }
 
 }
